a413d8eb3463b24fb0f668c9ac50680a1163bea7
Gem Kaiser Santiago <santiagogemkaiser@gmail.com>
2025-07-08 16:27:13 +0800

    Initial commit for URL Shortener Microservice

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +20,38 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urlDatabase = [];
+const dns = require("dns");
+const urlParser = require("url");
+
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+  const hostname = urlParser.parse(originalUrl).hostname;
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
+
+    const shortUrl = urlDatabase.length + 1;
+    urlDatabase.push({ original_url: originalUrl, short_url: shortUrl });
+
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const record = urlDatabase.find((entry) => entry.short_url === id);
+
+  if (record) {
+    res.redirect(record.original_url);
+  } else {
+    res.status(404).json({ error: "No short URL found for the given input" });
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
